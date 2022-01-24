5df906bc72d7546618a8cc5a3dd6f936a150ddb4
Francois Spies <fcspies@gmail.com>
2022-01-24 11:12:49 -0500

    bones

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 fetch('https://api.covidtracking.com/v1/states/current.json')
-    .then (r => r.json())
-    .then (console.log)
+.then(r => r.json())
+.then(data => console.log(data))
+
+
+
+
+
+function handleForm() {
+    const newComment = getELementById('comment-form')
+
+        fetch('', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+    console.log('Success:', data);
+    })
+}
